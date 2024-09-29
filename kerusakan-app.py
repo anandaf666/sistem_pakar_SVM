9a8d20793d63d9c054abde54957891a0a36463e8
Firdaus Ananda <101900626+anandaf666@users.noreply.github.com>
2024-09-29 13:08:37 +0700

    Add files via upload

--- kerusakan-app.py
+++ kerusakan-app.py
@@ -4,15 +4,23 @@ import numpy as np
 from sklearn.svm import SVC
 import pickle
 import matplotlib.pyplot as plt
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="ker_jar"
+)
+
+mycursor=mydb.cursor()
 
 st.write("""
          # Diagnosa Gangguan Jaringan Local Area Network SVM
          """)
 
 st.sidebar.header('Inputan User')
-
-
-
+nama = st.sidebar.text_input("Masukkan Nama")
 def input_user():
         G1 = st.sidebar.slider('(1.) SSID Wifi menghilang', 0.0, 1.0, 0.0)
         G2 = st.sidebar.slider('(2.) Password berubah', 0.0, 1.0, 0.0)
@@ -25,6 +33,7 @@ def input_user():
         G9 = st.sidebar.slider('(9.) Koneksi internet rendah', 0.0, 1.0, 0.0)
         G10 = st.sidebar.slider('(10.) Koneksi putus tiba-tiba tanpa adanya perubahan oleh admin', 0.0, 1.0, 0.0)
         G11 = st.sidebar.slider('(11.) Koneksi tidak stabil', 0.0, 1.0, 0.0)
+        
         data = {
             'G1' : G1,
             'G2' : G2,
@@ -47,6 +56,7 @@ kerusakan_raw = pd.read_csv('data_kerusakan.csv')
 kerusakan = kerusakan_raw.drop(columns=['Kerusakan'])
 
 df = pd.concat([inputan, kerusakan], axis=0)
+
 df = df[:1]
 st.subheader('Parameter Inputan')
 st.write(df)
@@ -55,10 +65,9 @@ load_model = pickle.load(open('model-kerusakan.pkl', 'rb'))
 prediksi = load_model.predict(df)
 prediksi_proba = load_model.predict_proba(df)
 
-
 st.subheader('Keterangan Label Class Kerusakan')
 
-kelas_kerusakan = np.array(['Internet Limit', 'Router Reset', 'Network Cable Unplugged', 'Router/Switch rusak'])
+kelas_kerusakan = np.array(['Router Reset', 'Internet Limit', 'Network Cable Unplugged', 'Router/Switch rusak'])
 st.write(kelas_kerusakan)
 
 st.subheader('Hasil Diagnosa')
@@ -66,7 +75,26 @@ st.write(kelas_kerusakan[prediksi])
 
 st.subheader('Probabilitas Hasil Diagnosa')
 st.write(prediksi_proba)
-
+hasl = max(max(prediksi_proba))
+st.write(hasl)
+nama = nama
+G1 = df['G1'].item()
+G2 = inputan['G2'].item()
+G3 = inputan['G3'].item()
+G4 = inputan['G4'].item()
+G5 = inputan['G5'].item()
+G6 = inputan['G6'].item()
+G7 = inputan['G7'].item()
+G8 = inputan['G8'].item()
+G9 = inputan['G9'].item()
+G10 = inputan['G10'].item()
+G11 = inputan['G11'].item()
+if st.sidebar.button("Create"):
+    sql= "insert into data_diagnosa(nama,G1,G2,G3,G4,G5,G6,G7,G8,G9,G10,G11,hasl) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    val= (nama,G1,G2,G3,G4,G5,G6,G7,G8,G9,G10,G11,hasl)
+    mycursor.execute(sql,val)
+    mydb.commit()
+    st.success("Record Created Successfully!!!")
 st.subheader('Solusi')
 if prediksi==[0]:
     st.write('Restart Router dan pastikan Kabel Jaringan benar terpasang dengan baik')
@@ -77,9 +105,4 @@ elif prediksi==[2]:
 else:
     st.write('Coba ganti dengan Switch atau Router yang baru dan dalam kondisi baik')
 
-target_mapper = {'Internet Limit':0, 'Router Reset':1, 'Network Cable Unplugged':2, 'Router/Switch Rusak':3}
-def target_encode(val):
-  return target_mapper[val]
-
-kerusakan_raw['Kerusakan'] = kerusakan_raw['Kerusakan'].apply(target_encode)
 
